9c5bd7dc1a5aca245441aa405092f16ca52476dc
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-20 14:19:20 -0400

    added copy constructor

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -3,34 +3,45 @@
 template <typename ItemType>
 DoublyLinkedList<ItemType>::DoublyLinkedList() : headPtr(nullptr), tailPtr(nullptr), itemCount(0) {}
 
-// template <typename ItemType>
-// DoublyLinkedList::DoublyLinkedList(const DoublyLinkedList<ItemType> &dList)
-// {
-//     /* Copy constructor:
-//         Copy headPtr, tailPtr, itemCount
-//     */
-//     if (dList.headPtr == nullptr)
-//     {
-//         headPtr = nullptr;
-//         tailPtr = nullptr;
-//         itemCount = 0;
-//     }
-//     else
-//     {
-//         //create headPtr
-//         headPtr = new DoubleNode<ItemType>();
-//         headPtr.setItem(dList.headPtr.getItem(), nullptr, nullptr);
+template <typename ItemType>
+DoublyLinkedList<ItemType>::DoublyLinkedList(const DoublyLinkedList<ItemType> &dList)
+{
+    /* Copy constructor:
+        Copy headPtr, tailPtr, itemCount
+    */
+    if (dList.headPtr == nullptr)
+    {
+        headPtr = nullptr;
+        tailPtr = nullptr;
+        itemCount = 0;
+    }
+    else
+    {
+        //create headPtr
+        headPtr = new DoubleNode<ItemType>();
+        headPtr.setItem(dList.headPtr.getItem(), nullptr, nullptr);
 
-//         //traverse through the rest of the list
-//         DoubleNode<ItemType> *origChainPtr = dList.headPtr;
-//         DoubleNode<ItemType> *travPtr = dList.headPtr;
-//         while (origChainPtr->getNext())
-//         {
-//             originChainPtr = origChainPtr->getNext();
-            
-//         }
-//     }
-// }
+        //traverse through the rest of the list
+        DoubleNode<ItemType> *origChainPtr = dList.headPtr;
+        DoubleNode<ItemType> *travPtr = dList.headPtr;
+        while (origChainPtr->getNext())
+        {
+            origChainPtr = origChainPtr->getNext();
+            DoubleNode<itemType> newNode = new DoubleNode<ItemType>;
+            newNode->setItem(origChainPtr->getItem());
+            newNode->setPrev(travPtr);
+            travPtr->setNext(newNode);
+            travPtr = travPtr->getNext();
+        }
+
+        //last node
+        DoubleNode<itemType> newNode = new DoubleNode<ItemType>;
+        newNode->setItem(origChainPtr->getItem());
+        travPtr->setItem(newNode);
+        newNode->setPrev(travPtr);
+        newNode->setNext(nullptr);
+    }
+}
 
 virtual ~DoublyLinkedList();
 bool insert(const ItemType &item, const int &position);
